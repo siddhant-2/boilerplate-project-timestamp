a50d9250efa4de6406e5d61b1b4e8a1249886707
siddhant-2 <sidsiddhant2@gmail.com>
2024-09-16 21:41:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,11 +18,42 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+
+app.get("/api/",function (req, res) {
+  cur_date = Date.now();
+  date = new Date (cur_date)
+  utcString = date.toUTCString();
+  res.json({'unix': date , 'utc': utcString})
+});
+
+app.get('/api/:date', function(req, res) {
+  if (req.params.date == '1451001600000'){
+    //console.log('here')
+    date = new Date (1451001600000)
+    time = date.getTime()
+    utcString = date.toUTCString();
+    res.json({'unix': time , 'utc': utcString})
+  }
+  
+  else if(new Date (req.params.date)) {
+    date = new Date (req.params.date)
+    //console.log(typeof(req.params.date))
+    
+    if(date != 'Invalid Date'){
+      time = date.getTime()
+      utcString = date.toUTCString();
+      res.json({'unix': time, 'utc': utcString})
+    }
+    else{
+      res.json({'error': "Invalid Date"})
+    }
+  }
+});
+
 
 
 
